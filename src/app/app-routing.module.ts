f0ab12626f96fa90ea5d9c6848586b204f2c9882
Atakan Arslan <atak.arslan@hotmail.com>
2023-10-21 12:29:08 +0300

    route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './admin/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: "admin", component: LayoutComponent, children: [
+
+    { path: "customers", loadChildren : ()=> import("./admin/components/customer/customer.module").then(module => module.CustomerModule)},
+    { path: "products", loadChildren : ()=> import("./admin/components/products/products.module").then(module => module.ProductsModule)},
+    { path: "orders", loadChildren : ()=> import("./admin/components/order/order.module").then(module => module.OrderModule)},
+    { path: "products", loadChildren : ()=> import("./admin/components/products/products.module").then(module => module.ProductsModule)},
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
